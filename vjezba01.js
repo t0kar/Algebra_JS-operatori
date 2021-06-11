a0e7a4271b273869ffcb507b3a524edc1a8abbf8
t0kar <81991067+t0kar@users.noreply.github.com>
2021-06-11 21:01:29 +0200

    update if esle

--- vjezba01.js
+++ vjezba01.js
@@ -1,17 +1,13 @@
-var x = 0;
+var x = 1;
 
 if (typeof x !== "number") {
     console.log("Unesena vrijednost nije broj! Ponovi unos.");
-} else {
-    if (x === 0) {
-        console.log("Uneseni broj je 0.");
-    } else {
-        if (x % 2 == 0) {
-            console.log("Broj", x, "je paran broj.");
-        } else if (x % 2 !== 0) {
-            console.log("Broj", x, "je neparan broj.");
-        }
-    }
+} else if (x === 0) {
+    console.log("Uneseni broj je 0.");
+} else if (x % 2 == 0) {
+    console.log("Broj", x, "je paran broj.");
+} else if (x % 2 !== 0) {
+    console.log("Broj", x, "je neparan broj.");
 }
 
 for (i = 1; i <= 100; i++) {
